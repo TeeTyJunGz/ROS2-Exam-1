72383621612e7cb0901ab3334a8349b176869ca1
Toonzaza <poppeth.peth@mail.kmutt.ac.th>
2024-09-14 23:25:35 +0700

    coppy_Scheduler

--- src/turtlesim_promax/scripts/coppy_scheduler.py
+++ src/turtlesim_promax/scripts/coppy_scheduler.py
@@ -15,15 +15,10 @@ class Scheduler(Node):
         self.declare_parameter('copy_name', ['Foxy','Noetic','Humble','Iron'])
         self.copy_name = self.get_parameter('copy_name').get_parameter_value().string_array_value
 
-        self.create_subscription(Bool, '/pizzaReady', self.pizzaReady_callback, 10)
-        self.create_subscription(Bool, '/savedReady', self.saveReady_callback, 10)
-        self.create_subscription(Bool, '/clearReady', self.clearReady_callback, 10)
-        self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
-        self.create_subscription(Bool,self.copy_name[0] + '/finished_tasks', self.finished_taks_callback_1, 10)
-        self.create_subscription(Bool,self.copy_name[1] + '/finished_tasks', self.finished_taks_callback_2, 10)
-        self.create_subscription(Bool,self.copy_name[2] + '/finished_tasks', self.finished_taks_callback_3, 10)
-        self.create_subscription(Bool,self.copy_name[3] + '/finished_tasks', self.finished_taks_callback_4, 10)
-
+        self.create_subscription(Bool, self.copy_name[0] + '/finished_tasks', self.finished_taks_callback_1, 10)
+        self.create_subscription(Bool, self.copy_name[1] + '/finished_tasks', self.finished_taks_callback_2, 10)
+        self.create_subscription(Bool, self.copy_name[2] + '/finished_tasks', self.finished_taks_callback_3, 10)
+        self.create_subscription(Bool, self.copy_name[3] + '/finished_tasks', self.finished_taks_callback_4, 10)
 
         self.state_publisher = self.create_publisher(String, 'state', 10)
         self.pizza_ready_publisher = self.create_publisher(String, 'PizzaReady', 10)
@@ -49,75 +44,34 @@ class Scheduler(Node):
                 self.copy_name = param.value
             else:
                 self.get_logger().warn(f'Unknown parameter: {param.name}')
-                # Return failure result for unknown parameters
                 return SetParametersResult(successful=False, reason=f'Unknown parameter: {param.name}')
-        # If all parameters are known, return success
         return SetParametersResult(successful=True)
     
     def finished_taks_callback_1(self, msg: Bool):
         self.finish_1 = msg.data
-        if msg.data == True:
+        if msg.data:
             self.go_right1 = True
 
     def finished_taks_callback_2(self, msg: Bool):
         self.finish_2 = msg.data
-        if msg.data == True:
+        if msg.data:
             self.go_right2 = True
 
     def finished_taks_callback_3(self, msg: Bool):
         self.finish_3 = msg.data
-        if msg.data == True:
+        if msg.data:
             self.go_right3 = True
 
     def finished_taks_callback_4(self, msg: Bool):
-        self.finish_3 = msg.data
-        if msg.data == True:
+        self.finish_4 = msg.data  # Corrected finish_4
+        if msg.data:
             self.go_right4 = True
     
-    def check(self, msg : Bool):
-        if self.go_right1 == True and self.go_right2 == True and self.go_right3 == True and self.go_right4 == True :
-            msg = Bool
+    def check(self):
+        if self.go_right1 and self.go_right2 and self.go_right3 and self.go_right4:
+            msg = Bool()
             msg.data = True
             self.go_right_publisher.publish(msg)
-    
-    def pizzaReady_callback(self, msg: Bool):
-        self.pizza_ready = msg.data
-        string_msg = String()
-        string_msg.data = 'pizzaReady'
-        self.pizza_ready_publisher.publish(string_msg)
-        
-    def clearReady_callback(self, msg: Bool):
-        self.clear_ready = msg.data
-    
-    def saveReady_callback(self, msg: Bool):
-        self.save_ready = msg.data
-        string_msg = String()
-        string_msg.data = 'savedReady'
-        self.save_ready_publisher.publish(string_msg)
-
-    def cmd_vel_callback(self, msg: Twist):
-        # Assign values from the Twist message
-        self.cmd_vel_x = msg.angular.x
-        self.cmd_vel_z = msg.angular.z
-
-    def state(self):
-        msg = String()  # Correct instantiation of String message
-        if self.clear_ready:
-            msg.data = 'clear'
-        else:
-            msg.data = 'teleop'
-        self.state_publisher.publish(msg)
-        # self.get_logger().info(f'Published state: {msg.data}')
 
     def timer_callback(self):
-        self.state()
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = Scheduler()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
+        self.check()
