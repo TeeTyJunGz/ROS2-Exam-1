c983495658e1ac28a15b8b06f46bbe5907c7a430
Toonzaza <poppeth.peth@mail.kmutt.ac.th>
2024-09-14 13:33:27 +0700

    state_01

--- src/turtlesim_promax/scripts/scheduler.py
+++ src/turtlesim_promax/scripts/scheduler.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python3
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Float64,Bool,String
 
-
-class DummyNode(Node):
+class Scheduler(Node):
     def __init__(self):
-        super().__init__('dummy_node')
+        super().__init__('scheduler_node')
+        self.create_subscription(Bool, '/pizzaReady', self.pizzaReady_callback, 10)
+        self.create_subscription(Bool, '/clearReady', self.clearReady_callback, 10)
+        self.state_publisher = self.create_publisher(String, '/state', 10)
+        self.create_timer(1.0 / self.freq, self.timer_callback)
+
+
+    def pizzaReady_callback(self,msg : Bool):
+        self.pizza_ready = msg.data
+
+    def clearReady_callback(self,msg : Bool):
+        self.clear_ready = msg.data
+
+    def state(self):
+        msg = String
+        if self.clear_ready == True:
+             msg.data = 'clear'
+        else:
+            msg.data = 'teleop'
+        self.state_publisher.publish(msg)
+
+    def timer_callback(self):
+            self.target()
+
+
+    
+
 
 def main(args=None):
     rclpy.init(args=args)
-    node = DummyNode()
+    node = Scheduler()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
